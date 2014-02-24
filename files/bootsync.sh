3b63dc153d181cdc5ec15bfcb038bf61e1ed87b5
YungSang <yungsang@gmail.com>
2014-02-24 14:00:34 +0900

    Start NFS client for NFS synced folder

--- files/bootsync.sh
+++ files/bootsync.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Start NFS client for NFS synced folder
+if [ -x /usr/local/etc/init.d/nfs-client ]; then
+  /usr/local/etc/init.d/nfs-client start
+fi
+
 # Place docker in /usr/bin for guest capability check
 ln -s /usr/local/bin/docker /usr/bin/docker
